65ce0c685deb9c6bd8e18dbbdb5d85b6fe022284
luca9294 <luca9294@hotmail.it>
2014-03-07 09:49:37 +0100

    TV-SHOWS Summary works

--- Television2/src/com/example/television2/TVShowActivity.java
+++ Television2/src/com/example/television2/TVShowActivity.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,16 +19,28 @@ public class TVShowActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tvshow);
 		TextView title =(TextView)findViewById(R.id.title);
-		TextView test =(TextView)findViewById(R.id.test);
+		TextView premiere =(TextView)findViewById(R.id.premiere);
+		TextView country =(TextView)findViewById(R.id.country);
+		TextView percentage =(TextView)findViewById(R.id.percentage);
+		TextView runtime =(TextView)findViewById(R.id.runtime);
+		TextView genre =(TextView)findViewById(R.id.genre);
+		TextView overview =(TextView)findViewById(R.id.overview);
 		ImageView image = (ImageView)findViewById(R.id.image);
 		ImageViewFromURL imageP;
 		
 
 			try {
 				
-				prova = new Tv_Show("Revenge",this);
+				prova = new Tv_Show("The walking dead",this);
 				
-				title.setText(prova.title);
+				title.setText(prova.title_n);
+				premiere.setText(prova.first_aired_iso);
+				country.setText(prova.country);
+				runtime.setText(prova.runtime + "m");
+				genre.setText(prova.genre);
+				percentage.setText(prova.percentage + "%");
+				overview.setText(prova.overview);
+				overview.setMovementMethod(new ScrollingMovementMethod());
 				imageP = new ImageViewFromURL (prova.image); 
 				image.setImageBitmap(imageP.getImage());
 				
